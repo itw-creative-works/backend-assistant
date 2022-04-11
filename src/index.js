e7b5673d1b34e3cd1ea8520521ee8a9ae0504cb0
Ian <ian.wiedenman@gmail.com>
2022-04-11 01:28:33 -0700

    Attach peroperties to error objects

--- src/index.js
+++ src/index.js
@@ -147,8 +147,15 @@ BackendAssistant.prototype.errorManager = function(e, options) {
   options.log = typeof options.log === 'undefined' ? true : options.log;
   options.sentry = typeof options.sentry === 'undefined' ? true : options.sentry;
   options.send = typeof options.send === 'undefined' ? true : options.send;
-  options.code = typeof options.code === 'undefined' ? 501 : options.code;
+  options.code = typeof options.code === 'undefined' ? 500 : options.code;
   const newError = e instanceof Error ? e : new Error(e);
+
+  // Attach properties
+  Object.keys(options)
+  .forEach((item, i) => {
+    Object.assign(newError , { [item]: options[item] })
+  });
+
 
   // Log the error
   if (options.log) {
